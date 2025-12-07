a8645bbc070a1f8d7f6054a13fbe01a8dd051129
romaisaKhurram <mahamkhurram1230@gmail.com>
2025-12-08 00:27:26 +0500

    feat(docusaurus): Update config and sidebars for GitHub Pages deployment

--- frontend/docusaurus.config.ts
+++ frontend/docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://romaisakhurram.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Ai-Native-Book-Course/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: '', // Usually your GitHub org/user name.
-  projectName: '', // Usually your repo name.
+  organizationName: 'romaisakhurram', // Usually your GitHub org/user name.
+  projectName: 'Ai-Native-Book-Course', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
