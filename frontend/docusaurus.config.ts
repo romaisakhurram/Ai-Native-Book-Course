3c8e94e109716c0ee816ee4373c22d97c4c94675
romaisaKhurram <mahamkhurram1230@gmail.com>
2025-12-19 01:47:59 +0500

    Revert onBrokenLinks to 'warn' after fixing routing

--- frontend/docusaurus.config.ts
+++ frontend/docusaurus.config.ts
@@ -25,7 +25,7 @@ const config: Config = {
   organizationName: 'romaisakhurram', // Usually your GitHub org/user name.
   projectName: 'Ai-Native-Book-Course', // Usually your repo name.
 
-  onBrokenLinks: 'ignore',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
