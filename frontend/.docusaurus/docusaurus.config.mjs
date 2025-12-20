1e8543072d785d3d51afabeddf56c9377253d9fc
romaisaKhurram <mahamkhurram1230@gmail.com>
2025-12-20 23:22:06 +0500

    update

--- frontend/.docusaurus/docusaurus.config.mjs
+++ frontend/.docusaurus/docusaurus.config.mjs
@@ -29,7 +29,7 @@ export default {
     "experimental_router": "browser"
   },
   "url": "https://romaisakhurram.github.io",
-  "baseUrl": "/Ai-Native-Book-Course/",
+  "baseUrl": "/",
   "organizationName": "romaisakhurram",
   "projectName": "Ai-Native-Book-Course",
   "onBrokenLinks": "warn",
