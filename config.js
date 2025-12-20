d90eac85f126ca703e0cfd13f5ee7eb029459fb8
romaisaKhurram <mahamkhurram1230@gmail.com>
2025-12-20 23:14:21 +0500

    update

--- config.js
+++
@@ -1,6 +0,0 @@
-// Configuration file for the AI Native Book Course
-window.BACKEND_CONFIG = {
-  apiUrl: 'https://romaisakhurram-deploy-project.hf.space',
-  defaultModel: 'gpt-3.5-turbo',
-  maxTokens: 1000
-};
